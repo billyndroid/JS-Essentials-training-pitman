8f7e5a58b88ac607ee0a5ca2c658844cd5b64fef
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-02-21 13:15:51 +0000

    update

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+let carName = "Porsche";
+let carTopSpeed = "200mph";
+
+document.getElementById("nav-logo");
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+document.getElementById("demo").innerHTML = "Hello World Again";
